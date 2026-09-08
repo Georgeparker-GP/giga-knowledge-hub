84d341ffd80bd8c0909e6a21df8e05563e16b386
Giga Parkaia <gigaparkaia@gmail.com>
2026-09-08 18:17:35 +0400

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='gkh-v4-cache';const ASSETS=['./','./index.html','./seed.json','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
