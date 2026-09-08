ad65513aa1f4595a8d1afef2841051ce5b6e3b24
Giga Parkaia <gigaparkaia@gmail.com>
2026-09-08 22:06:43 +0400

    Align cloud config version with V6.1

--- supabase-config.js
+++ supabase-config.js
@@ -3,5 +3,5 @@ window.GKH_SUPABASE = {
   url: 'https://frzjfgwtubgfqkajlxri.supabase.co',
   publishableKey: 'sb_publishable_TfJc5FmfwXJAQyl_7sm8fw_rhkAIxCh',
   bucket: 'gkh-files',
-  version: '6.0'
+  version: '6.1'
 };
